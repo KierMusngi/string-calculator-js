214653eea88c3f27fb6d3d86076210d8245f2351
tir.kier <kier@tirsolutions.com>
2019-11-21 15:16:47 +0800

    added javascript debugger and references

--- src/string-calculator.js
+++ src/string-calculator.js
@@ -1,10 +1,21 @@
 class StringCalculator{
     constructor(){
-
     }
 
-    Add(stringNumber){
-        return 0;
+    Add(input){
+        if (input === ""){
+            return 0;
+        }
+
+        let total = 0;
+        let stringNumbers = input.split(",");
+
+        stringNumbers.forEach(stringNumber => {
+            let integer = parseInt(stringNumber);
+            total += integer;
+        });
+
+        return total;
     }
 };
 
--- test/string-calculator.test.js
+++ test/string-calculator.test.js
@@ -3,12 +3,47 @@ import StringCalculator from '../src/string-calculator';
 test('Add empty string', () =>
 {
     // Arrange
-    let sum;
     let calculator = new StringCalculator();
 
     // Act
-    sum = calculator.Add("");
+    let sum = calculator.Add("");
 
     // Assert
     expect(sum).toEqual(0);
+});
+
+test('Add single string number', () => 
+{
+    // Arrange
+    let calculator = new StringCalculator();
+
+    // Act
+    let sum = calculator.Add("1");
+    
+    // Assert
+    expect(sum).toEqual(1);
+});
+
+test('Add two string number delimited with comma', () => 
+{
+    // Arrange
+    let calculator = new StringCalculator();
+
+    // Act
+    let sum = calculator.Add("1,2");
+
+    // Assert
+    expect(sum).toEqual(3);
+});
+
+test('Add unknown amount of string numbers delimited with commas', () => 
+{
+    // Arrange
+    let calculator = new StringCalculator();
+
+    // Act
+    let sum = calculator.Add("1,2,3,4,5,6,7,8,9,10");
+
+    // Assert
+    expect(sum).toEqual(55);
 });
